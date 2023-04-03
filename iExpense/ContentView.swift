a4410cee019a1617a32204686fcddba968b9d32b
SeikaHirori <SeikaHirori@outlook.com>
2023-04-03 14:04:25 -0700

    Demo'd how @State and struct works in SwiftUI

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        return VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            
+            new_tools_part_1()
+            
         }
         .padding()
+        
+        
     }
 }
 
@@ -23,4 +28,20 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct new_tools_part_1: View {
+    @State private var user:User = User()
+    
+    var body: some View {
+        return VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
+    }
+
+    
 }
