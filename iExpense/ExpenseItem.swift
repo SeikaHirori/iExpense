2e48d5e6a29939540e334be5b572b7bdfd7e6abe
SeikaHirori <SeikaHirori@outlook.com>
2023-04-04 02:23:27 -0700

    added

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,12 @@
+
+struct ExpenseItem {
+
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+
+    @Published var items:[ExpenseItem] = [ExpenseItem]()
+}
